7c42c4cd0f61348632030a15ab3b5f00ef26bc14
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-11-11 21:25:53 +0530

    second

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function (req, res) {
 //create operation
 app.get("/create", async function (req, res) {
     const user = await userModel.create({
-        name: "pinso",
+        name: "abhi",
         email: "patel@err",
         age: 23,
     })
@@ -19,7 +19,7 @@ app.get("/create", async function (req, res) {
 });
 app.get("/createSel", async function (req, res) {
     const sel = await selModel.create({
-        product: "pinso",
+        product: "abhi",
         prize: 230,
 
     })
@@ -36,5 +36,20 @@ app.get("/user/:username", async function (req, res) {
     const user = await userModel.findOne({ name: req.params.username });
     res.send(user);
 });
+// do update the value
+app.get('/update/:username',async function(req,res){
+    let updated=await userModel.findOneAndUpdate({name:"abhi"},{name:"abhi patel"},{new:true});
+    res.send(updated)
+});
+// app.get('/update/:username',async function(req,res){
+//     let user=await userModel.findOneAndUpdate({name:req.params.username},{name:"arsh"});
+//     user.name=user.name,
+//     await user.save();
+//     res.send(user)
+// });
 
+app.get('/delete',async function(req,res){
+    let deleted= await userModel.findOneAndDelete({name:"arsh"});
+    res.send(deleted)
+})
 app.listen(3000);
