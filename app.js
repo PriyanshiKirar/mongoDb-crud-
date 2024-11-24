a5537fa6199b505cda2d45dde2fe0643c60bf150
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-11-24 17:19:28 +0530

    24nov

--- app.js
+++ app.js
@@ -79,12 +79,19 @@ const app = express();
 const userModel = require("./model/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const expressSesion=require("express-session");
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'sdfghjklll',
+    resave: false,
+    saveUninitialized: true,
+}))
+
 app.get("/", function (req, res) {
     res.render("register");
 })
