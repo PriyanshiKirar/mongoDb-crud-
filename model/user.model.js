aeede76d9b56d6e56144941c828ccfc3a34fef7d
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2024-11-26 22:50:31 +0530

    26nov

--- model/user.model.js
+++ model/user.model.js
@@ -2,7 +2,9 @@ const mongoose=require("mongoose");
 
 const userSchema=mongoose.Schema({
     name:String,
+    firstName:String,
     email:String,
     password:String,
+    age:Number,
 })
 module.exports=mongoose.model("user",userSchema);
